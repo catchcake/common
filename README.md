c095d7a0ad26ede25126a11f3e46d24555cb8f81
Jindrich K. Smitka <jsmitka@smita.info>
2021-06-21 15:25:59 +0200

    Init...

---
+++ README.md
@@ -0,0 +1,20 @@
+# Common
+> TODO: Add description**
+
+
+## Installation
+
+```elixir
+def deps do
+  [
+    {:common, "~> 0.1.0"}
+  ]
+end
+```
+
+## License
+
+> TODO: Add license
+
+----
+Created:  2021-06-21Z
---
+++ mix.exs
@@ -0,0 +1,44 @@
+defmodule Common.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :common,
+
+      version: "0.1.0",
+      elixir: "~> 1.8",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ex_doc, "~> 0.21", only: :dev},
+      {:credo, "~> 1.2", only: [:dev, :test]},
+      {:excoveralls, "~> 0.13", only: :test},
+
+    ]
+  end
+end
