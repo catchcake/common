727af929385e4e1af9deccba970060e980a0c7c8
Jindrich K. Smitka <jsmitka@smita.info>
2021-07-12 15:08:21 +0200

    Added context update_in

--- lib/catchcake_common/context.ex
+++ lib/catchcake_common/context.ex
@@ -5,15 +5,17 @@ defmodule CatchCakeCommon.Context do
 
   alias CatchCakeCommon.MapExtra
 
+  @type context() :: map()
+
   @doc """
   Returns a new empty map.
 
   ## Examples
-      iex> new()
+      iex> Context.new()
       %{}
 
   """
-  @spec new() :: map()
+  @spec new() :: context()
   def new() do
     Map.new()
   end
@@ -24,14 +26,14 @@ defmodule CatchCakeCommon.Context do
 
   ## Examples
 
-      iex> new([{:b, 1}, {:a, 2}])
+      iex> Context.new([{:b, 1}, {:a, 2}])
       %{a: 2, b: 1}
 
-      iex> new(a: 1, a: 2, a: 3)
+      iex> Context.new(a: 1, a: 2, a: 3)
       %{a: 3}
 
   """
-  @spec new(Enumerable.t()) :: map()
+  @spec new(Enumerable.t()) :: context()
   def new(enum) do
     Map.new(enum)
   end
@@ -41,14 +43,14 @@ defmodule CatchCakeCommon.Context do
 
   ## Examples
       iex> context = %{a: 1, b: %{c: 2}}
-      iex> derive(context, :d, [:b, :c], fn v -> v * 10 end)
+      iex> Context.derive(context, :d, [:b, :c], fn v -> v * 10 end)
       %{a: 1, b: %{c: 2}, d: 20}
 
       iex> context = %{a: 1, b: %{c: 2}}
-      iex> derive(context, :d, [:b, :c])
+      iex> Context.derive(context, :d, [:b, :c])
       %{a: 1, b: %{c: 2}, d: 2}
   """
-  @spec derive(map(), Map.key(), [Map.key()], (any() -> any())) :: map()
+  @spec derive(context(), Map.key(), [Map.key()], (any() -> any())) :: context()
   def derive(context, key, path, transform \\ &Function.identity/1)
       when is_map(context) and is_list(path) and is_function(transform, 1) do
     Map.put(
@@ -63,14 +65,27 @@ defmodule CatchCakeCommon.Context do
 
   ## Examples
 
-      iex> put(%{a: 1, b: 2}, :c, fn %{a: a, b: b} -> a + b end)
+      iex> Context.put(%{a: 1, b: 2}, :c, fn %{a: a, b: b} -> a + b end)
       %{a: 1, b: 2, c: 3}
 
-      iex> put(%{a: 1, b: 2}, :d, fn _ -> 50 end)
+      iex> Context.put(%{a: 1, b: 2}, :d, fn _ -> 50 end)
       %{a: 1, b: 2, d: 50}
   """
-  @spec put(map(), Map.key(), (map() -> any())) :: map()
+  @spec put(context(), Map.key(), (context() -> any())) :: context()
   def put(context, key, f) do
     Map.put(context, key, f.(context))
+  end
+
+  @doc """
+  Updates a key in a nested context structure.
+
+  ## Examples
+      iex> context = %{a: %{b: 1}, c: 3, d: 4}
+      iex> Context.update_in(context, [:a, :b], fn ctx -> ctx.a.b + ctx.c + ctx.d end)
+      %{a: %{b: 8}, c: 3, d: 4}
+  """
+  @spec update_in(context(), nonempty_list(Map.key()), (context() -> any())) :: context()
+  def update_in(context, path, f) do
+    MapExtra.update_in(context, path, fn _ -> f.(context) end)
   end
 end
